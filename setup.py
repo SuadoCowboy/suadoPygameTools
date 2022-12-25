8e26c5d51999bba317a1ace31290da9d2f385b7a
SuadoCowboy <luccacsg@gmail.com>
2022-12-25 20:56:23 -0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='pygameTools',
+version='0.1',
+description='pygameTools is a package with the purpose of helping to create simple 2d pygame games.',
+url='https://github.com/SuadoCowboy/pygameTools',
+author='Suado Cowboy',
+license='MIT',
+packages=['pygameTools'],
+install_requires=[
+    'pygame'
+],
+zip_safe=False
+)
